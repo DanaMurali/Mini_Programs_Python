f35c9e8e73ed26de097ec7dcf2f5174ea019acee
Darania Muralitharan <Darania@daranias-air.home>
2022-01-09 21:47:24 +0000

    Return the count of how many times the sub-string “Katie” appears in the given string

---
+++ Basics/Question7.py
@@ -0,0 +1,6 @@
+# Return the count of how many times the sub-string “Katie” appears in the given string
+
+string = "Katie is good developer. Katie is a writer"
+
+count = string.count("Katie")
+print(count)
