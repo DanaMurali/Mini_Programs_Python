64cf71f3dde6f6df244925131f4883521fc5ad18
Darania Muralitharan <Darania@daranias-air.home>
2022-01-09 17:53:49 +0000

    Print the result of the multiplication of two sets of integers if it is less than 1000. If the result is greater than 1000, print their sum.

---
+++ Basics/Question1.py
@@ -0,0 +1,22 @@
+# Print the result of the multiplication of two sets of integers if it is less than 1000. If the result is greater than 1000, print their sum.
+
+num1 = 25
+num2 = 26
+
+
+num1 = 35
+num2 = 36
+
+def multiplication_or_addition(num1, num2):
+    product = num1 * num2 
+
+    if product < 1000:
+        return product
+    else:
+        return num1 + num2
+
+result = multiplication_or_addition(25,26)
+print(result)
+
+result = multiplication_or_addition(120, 123)
+print(result)
