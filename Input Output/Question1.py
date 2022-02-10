92182dfcbc25c01125309ca0d467ae7b5d6c6d29
Darania Muralitharan <Darania@Daranias-MacBook-Air.local>
2022-02-10 12:05:55 +0000

    Accept two numbers from the user and calculate multiplication

---
+++ Input Output/Question1.py
@@ -0,0 +1,8 @@
+# Accept two numbers from the user and calculate multiplication
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = num1 * num2
+
+print(result)
